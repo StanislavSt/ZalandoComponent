b07c7c0d2ad128c28b8cf9ad1f9bb1dea88db03b
StanislavSt <s.staev1@gmail.com>
2020-01-02 12:37:41 +0100

    Moved data to props

--- src/components/ZalandoMenu.vue
+++ src/components/ZalandoMenu.vue
@@ -27,38 +27,12 @@
 
 <script>
 export default {
+	props: ["menuItems", "images", "rawHtml"],
 	data() {
 		return {
 			isMounted: false,
 			currentindex: 1,
-			slideDirection: "left",
-			//Dynamic Data
-			menuItems: ["beschrijving", "fitting", "together", "WITHMEISYOU"],
-			images: [
-				"https://img02.ztat.net/article/RO/54/1E/03/GN/11/RO541E03G-N11@12.jpg?impolicy=detail",
-				"https://img01.ztat.net/article/RO/54/1E/03/KQ/11/RO541E03K-Q11@9.jpg?impolicy=detail",
-				"https://img01.ztat.net/article/RO/54/1E/03/KE/11/RO541E03K-E11@9.jpg?impolicy=detail",
-				"https://mosaic01.ztat.net/vgs/static/pdp_review_3.jpg"
-			],
-			rawHtml: [
-				`		<p>MATERIAAL & WASVOORSCHRIFT</p>
-						<p>Materiaal buitenlaag:</p>
-						<p>Materiaalverwerking:</p>
-						<p>Wasvoorschrift:</p>`,
-				`				<p>Lichaamslengte model:</p>
-								<p>Lengte:</p>
-								<p>Mouwlengte:</p>
-								<p>Mouwlengte:</p>
-								<p>Totale lengte:</p>`,
-				`								<p>Gratis verzending en retour</p>
-												<p>100 dagen recht op retour</p>`,
-				`				<p>Dit artikel heeft nog geen beoordelingen. Wil jij de eerste zijn?</p>
-								<button class="mening-button">
-									<div class="button-text-container">
-										<span>DEEL JOUW ERVARING</span>
-									</div>
-								</button>`
-			]
+			slideDirection: "left"
 		};
 	},
 	methods: {
